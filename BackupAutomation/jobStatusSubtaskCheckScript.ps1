e5f99b0c036a0ea990204cb393ac3380505e5f73
zukako <shizhong.hangxi@gmail.com>
2022-02-03 11:57:08 +0900

    add subtask script

---
+++ BackupAutomation/jobStatusSubtaskCheckScript.ps1
@@ -0,0 +1,27 @@
+# リソースグループ、リカバリサービスボルト名指定
+$rgname = "AAAM-rg" 
+$rsname = "AAAM-rsvault"
+$vault = Get-AzRecoveryServicesVault -ResourceGroupName $rgname -Name $rsname
+
+$Jobs = Get-AzRecoveryServicesBackupJob -Status InProgress -VaultId $vault.ID
+$Jobs
+
+$tscount = $Jobs.Count
+while ($tscount -gt 0) {
+    Write-Host "${tscount} Jobs are Taking Snapshot..."
+    foreach ($Job in $Jobs) {
+        $wlname = $Job.WorkloadName
+        $SubTasks = Get-AzRecoveryServicesBackupJobDetail -Job $Job -VaultId $Vault.id
+        # subtasks[0] = Take Snapshot
+        if($SubTasks.subtasks[0].Status -ne "Completed"){
+            # $stname = $SubTasks.subtask[0].Name
+            Write-Host "${wlname} is Taking Snapshot..."
+            $SubTasks.subtasks
+        }else{
+            $tscount--
+        }
+    }
+    Start-Sleep -Seconds 10
+}
+
+Write-Host "(Subtask)Taking Snapshot process is ended"
