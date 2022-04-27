674aba616433d05e31e6a553dc08771798e99bc2
zukako <shizhong.hangxi@gmail.com>
2022-04-27 17:52:54 +0900

    translated

--- 20220427_PSscript/invokeCommandScript.ps1
+++ 20220427_PSscript/invokeCommandScript.ps1
@@ -1,4 +1,4 @@
-$Return = Invoke-Command localhost -ScriptBlock {ls} -AsJob
+$Return = Invoke-Command {localhost} -ScriptBlock {dir c:\} -AsJob
 # echo $Return
 Get-Job
 # Get Jobs
@@ -11,7 +11,7 @@ do{
     # Left-over Jobs
     if( $RunningJobs -ne $null ){
         $Now = Get-Date
-        $Message = "未処理ジョブ " + $Now
+        $Message = "Jobs bellow are running now: " + $Now
         echo $Message
         $RunningJobs | Format-Table -Property Id,Name,State,Location -AutoSize | Out-Host
         echo ""
