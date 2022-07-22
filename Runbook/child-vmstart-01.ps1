27a3f807d92059ccc2e4eb34b647446d9e53b774
zukako <shizhong.hangxi@gmail.com>
2022-07-22 09:42:49 +0900

    custom object

--- Runbook/child-vmstart-01.ps1
+++ Runbook/child-vmstart-01.ps1
@@ -1,4 +1,12 @@
 $AzureContext = (Connect-AzAccount -Identity).context
 # set and store context
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContex
-Start-AzVM -ResourceGroupName "HITACHI-DEMO-rg" -Name "20220428testvm"
+$result = Start-AzVM -ResourceGroupName "HITACHI-DEMO-rg" -Name "20220428testvm"
+# カスタムオブジェクトを返したい場合
+$myObject = [PSCustomObject]@{
+    # パラメータを渡している場合はそこから必要な値を抽出
+    # Name = $VM.Name
+    Name = "20220428testvm"
+    Status = $result.Status
+}
+Write-Output $myObject
--- Runbook/child-vmstart-02.ps1
+++ Runbook/child-vmstart-02.ps1
@@ -1,4 +1,5 @@
 $AzureContext = (Connect-AzAccount -Identity).context
 # set and store context
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
-Start-AzVM -ResourceGroupName "HITACHI-DEMO-rg" -Name "20220428testvm02"
+$result = Start-AzVM -ResourceGroupName "HITACHI-DEMO-rg" -Name "20220428testvm02"
+Write-Output $result
